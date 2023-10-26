d91462b618b1a8c4c316ebb43939114c47848a97
sheldonu <uch20002@byui.edu>
2023-10-25 20:13:01 -0700

    mistake

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 
 app.get('/', (req,res) => {res.send("Hello Browser")});
 
-app.get('/sheldon', (req,res) => {res.send("Hello Sheldon")});
+app.get('/sheldon', (req,res) => {res.send("Hello Sheldo")});
 
 app.listen(port, ()=>console.log("Listening"));
